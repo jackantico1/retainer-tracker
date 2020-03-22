841b49e498f6cdd9330456f0963462c4ef30130f
Jack Antcio <anticoj@bc.edu>
2020-03-22 14:47:13 -0400

    Initial Commit

---
+++ retainer-tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  retainer-tracker
+//
+//  Created by Jack Antico on 3/22/20.
+//  Copyright © 2020 Jack Antico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ retainer-trackerTests/retainer_trackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  retainer_trackerTests.swift
+//  retainer-trackerTests
+//
+//  Created by Jack Antico on 3/22/20.
+//  Copyright © 2020 Jack Antico. All rights reserved.
+//
+
+import XCTest
+@testable import retainer_tracker
+
+class retainer_trackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
